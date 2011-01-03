402733f1ffc3a3af704acac80253b0462cedebbf
Jon Parise <jon@indelible.org>
2011-01-02 20:53:32 -0800

    Adding my basic Mercurial configuration file

--- bin/link.sh
+++ bin/link.sh
@@ -5,6 +5,9 @@ LN='ln -fhsv'
 # Git
 $LN $HOME/.dotfiles/.gitconfig $HOME/.gitconfig
 
+# Mercurial
+$LN $HOME/.dotfiles/.hgrc $HOME/.hgrc
+
 # Vim
 $LN $HOME/.dotfiles/.vim $HOME/.vim
 $LN $HOME/.dotfiles/.vimrc $HOME/.vimrc
