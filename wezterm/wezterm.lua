4689975e81c3ee247bb251d2d7777de9537c3df4
Jon Parise <jon@indelible.org>
2023-12-27 17:07:25 -0800

    wezterm: switch to Iosevka Fixed
    This variant doesn't include ligatures so there's no need to disable
    those font features

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,16 +6,15 @@ config = wezterm.config_builder()
 -- Appearance
 config.color_scheme = 'Oceanic-Next'
 config.font = wezterm.font {
-    family = 'Iosevka Term', 
+    family = 'Iosevka Fixed',
     weight = 'Light',
-    harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
 }
 config.font_size = 15.0
 config.initial_cols = 130
 config.initial_rows = 70
 config.window_decorations = "RESIZE"
 config.window_frame = {
-    font = wezterm.font 'Iosevka Term',
+    font = wezterm.font 'Iosevka Fixed',
     font_size = 13.0,
 }
 config.window_padding = {
